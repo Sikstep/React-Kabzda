3d7c7fc29b3ed61a3446a4185e0a69a495d50574
Sikstep <Sikstep1988@gmail.com>
2023-01-26 14:32:57 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+          <div>one div</div>
+            <div className="App">
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>                    A
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
