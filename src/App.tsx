8f2e14ee604b0c49e44210e23c98287a203dcfda
Sikstep <Sikstep1988@gmail.com>
2023-01-27 16:14:18 +0300

    Lesson - 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import Tehnologies from "./Tehnologies";
+
 
 const App = () => {
     return (
         <div>
-          <Header />
-            <Tehnologies />
-
+            <Header/>
+            <Tehnologies/>
         </div>
     );
 }
 
-const Tehnologies = () => {
-    return (<div>
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>react</li>
-            <li>react2</li>
-        </ul>
-    </div>
-    );
-}
-const Header = () => {
-    return (<div>
-            <a href={'#s'}>Home</a>
-            <a href={'#s'}>News Feed</a>
-            <a href={'#s'}>Messages</a>
-        </div>
-            );
-}
+
+
 export default App;
---
+++ src/Tehnologies.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Tehnologies = () => {
+    return (<div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>react</li>
+                <li>react2</li>
+            </ul>
+        </div>
+    );
+}
+export default Tehnologies;
