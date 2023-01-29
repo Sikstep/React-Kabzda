6f61ce8caa8f3cd63cd0c973eb2fb2c939efe95c
Sikstep <Sikstep1988@gmail.com>
2023-01-29 20:45:09 +0300

    02 React Kabzda

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Tehnologies from "./Tehnologies";
 
+function hello() {
+
+    alert('Hello IT-Kamasutra')
+}
+
+// hello();
 
 const App = () => {
+    // полезное что - то
+
+    // обязательно вернуть JSX
     return (
         <div>
-            <Header/>
-            <Tehnologies/>
+            I love you
+            <Rating/>
+            <Rating/>
+            <Accordion/>
         </div>
     );
 }
 
+function Rating() {
 
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Accordion() {
+
+    return <div>
+        <h3>Меню</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+}
+
+function Star ()  {
+    return <div>Star</div>
+}
 
 export default App;
--- src/User/user.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export const User = () => {
-    return (
-        <div>
-            <h2>User</h2>
-            <h2>User2</h2>
-        </div>
-    );
-}
