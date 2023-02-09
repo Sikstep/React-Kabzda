a3576cd228c32c20b80349c2c8ae6f5eae20e02f
Sikstep <Sikstep1988@gmail.com>
2023-02-09 16:22:40 +0300

    Условный рендеринг React Kabzda

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import classes from './App.module.css';
 import {Fullinput} from './components/Fullinput';
 import {Button} from './components/Button';
 import {Input} from './components/Input';
+import Accordion from './components/Accordion';
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         {message: 'message2'},
         {message: 'message3'},
     ]);
+    let arrForAccordion = [1,2,3];
 
     const [msg, setMsg] = useState('');
 
@@ -29,13 +31,15 @@ const App = () => {
     return (
         <div className={classes.App}>
             {/*<Fullinput buttomName={'+'} buttomClick={addMessage}/>*/}
-            <Input msg={msg} setMsg={setMsg}/>
-            <Button name={'+'} callBack={newMessageAdd}/>
-            {message.map((el, index) => {
-                return (
-                    <div key={index}>{el.message}</div>
-                )
-            })}
+            {/*<Input msg={msg} setMsg={setMsg}/>*/}
+            {/*<Button name={'+'} callBack={newMessageAdd}/>*/}
+            {/*{message.map((el, index) => {*/}
+            {/*    return (*/}
+            {/*        <div key={index}>{el.message}</div>*/}
+            {/*    )*/}
+            {/*})}*/}
+            <Accordion titleValue={'--Menu--'} body={arrForAccordion} collapsed={false}/>
+            <Accordion titleValue={'--Users--'} body={arrForAccordion} collapsed={false}/>
         </div>
     );
 }
