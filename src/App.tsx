055990bc740d6ce801583a98da0b83d083717555
Sikstep <Sikstep1988@gmail.com>
2023-02-14 22:16:44 +0300

    Условный рендеринг React Kabzda + uncontrolled Rating finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React, {useState} from 'react';
 import classes from './App.module.css';
-import {Fullinput} from './components/Fullinput';
-import {Button} from './components/Button';
-import {Input} from './components/Input';
-import Accordion from './components/Accordion/Accordion';
 import {Rating} from './components/Rating/Rating';
-import { OnOffButton } from './components/OnOff/OnOff';
+import {OnOffButton} from './components/OnOff/OnOff';
 import {UncontrelledAccordion} from './components/Accordion/UncontrolledAccordion';
-
 
 
 const App = () => {
@@ -17,7 +12,7 @@ const App = () => {
         {message: 'message2'},
         {message: 'message3'},
     ]);
-    let arrForAccordion = [1,2,3];
+    let arrForAccordion = [1, 2, 3];
 
     const [msg, setMsg] = useState('');
 
@@ -34,33 +29,12 @@ const App = () => {
 
     return (
         <div className={classes.App}>
-            {/*<Fullinput buttomName={'+'} buttomClick={addMessage}/>*/}
-            {/*<Input msg={msg} setMsg={setMsg}/>*/}
-            {/*<Button name={'+'} callBack={newMessageAdd}/>*/}
-            {/*{message.map((el, index) => {*/}
-            {/*    return (*/}
-            {/*        <div key={index}>{el.message}</div>*/}
-            {/*    )*/}
-            {/*})}*/}
-            {/*<Accordion titleValue={'--Menu--'} collapsed={true}/>*/}
+
             {/*<Accordion titleValue={'--Users--'} collapsed={true}/>*/}
-            <Rating />
-            {/*<Rating />*/}
-            {/*<Rating />*/}
-            {/*<Rating />*/}
-            {/*<Rating />*/}
-            {/*<Rating />*/}
-            {/*<Rating value={1}/>*/}
-            {/*<Rating value={2}/>*/}
-            {/*<Rating value={3}/>*/}
-            {/*<Rating value={4}/>*/}
-            {/*<Rating value={5}/>*/}
-            <OnOffButton />
-            <OnOffButton />
-            <OnOffButton />
-            <OnOffButton />
-            {/*<OnOffButton />*/}
-            {/*<OnOffButton />*/}
+            <Rating/>
+
+            <OnOffButton/>
+
             <UncontrelledAccordion titleValue={'Menu'}/>
 
         </div>
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -22,13 +22,10 @@ export function Rating(props: RatingPropsType) {
 
 type StarPropsType = {
     selected: boolean
+
 }
 
 function Star(props: StarPropsType) {
 
-    if (props.selected === true) {
-        return <span><b>Star</b></span>
-    } else {
-        return <span>Star</span>
-    }
+   return props.selected ? <span><b>Star</b></span> : <span>Star</span>
 }
