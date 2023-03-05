0ab177ef3eb1e0023591be16241ce5b90370342f
Sikstep <Sikstep1988@gmail.com>
2023-03-05 18:48:02 +0300

    Implementation Storybook +OnOff Component start

--- src/components/OnOff/OnOff.stories.tsx
+++ src/components/OnOff/OnOff.stories.tsx
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
 import {OnOffButton} from './OnOff';
+import {action} from '@storybook/addon-actions';
 
 export default {
   title: 'Controlled OnOff',
   component: OnOffButton,
 }
 
-export const SetOn = () => <OnOffButton value={true} setValue={x=>x}/>;
-export const SetOff = () => <OnOffButton value={false} setValue={x=>x}/>;
+const callBack = action('on or off clicked')
+export const SetOn = () => <OnOffButton value={true} setValue={callBack}/>;
+export const SetOff = () => <OnOffButton value={false} setValue={callBack}/>;
 export const OnOffChanging = () => {
   const [value, setValue] = useState(true);
   return <OnOffButton value={value} setValue={setValue}/>
