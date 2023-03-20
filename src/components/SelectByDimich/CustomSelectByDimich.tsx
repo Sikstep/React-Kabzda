c486efc17e57d953d42fe2fd36d200bbbe2bd5df
Sikstep <Sikstep1988@gmail.com>
2023-03-20 22:31:55 +0300

    Controlled Dimych customSelect writing+

--- src/components/SelectByDimich/CustomSelectByDimich.tsx
+++ src/components/SelectByDimich/CustomSelectByDimich.tsx
@@ -11,20 +11,30 @@ type CustomSelectByDimichType = {
 const CustomSelectByDimich = (props: CustomSelectByDimichType) => {
 
     const [active, setActive] = useState(false);
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value);
+
     const selectedItem = props.items.find(i => i.value === props.value);
+    const hoveredItem = props.items.find(i => i.value === hoveredElementValue);
     const toggleItem = () => setActive((active) => !active)
     const itemCliched = (value: number) => {
         props.onChange(value);
+        setActive((active) => !active)
     }
-
+    const onKeyPresshandler = (e: number) => {
+        console.log('pressed')
+    }
     return (
         <>
-            <div className={s.select}>
+            <div
+                tabIndex={0}
+                className={s.select}>
                 <span className={s.main} onClick={toggleItem}>{selectedItem && selectedItem.title}</span>
                 {
                     active &&
                     <div className={s.items}>
                         {props.items.map(el => <div
+                            onMouseEnter={()=>setHoveredElementValue(el.value)}
+                            className={s.item + ' ' + (hoveredItem === el ? s.selected : '')}
                             key={el.value}
                             onClick={() => itemCliched(el.value)}
                         >
