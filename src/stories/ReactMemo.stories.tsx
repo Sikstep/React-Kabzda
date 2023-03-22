bd4e476475bbf6055edcc0e81d0395ba2df5d50d
Sikstep <Sikstep1988@gmail.com>
2023-03-22 15:46:53 +0300

    ReactMemo started. Created new func NewMessagesCounter & Users

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -1,3 +1,5 @@
+import {useState} from 'react'
+
 export default {
     title: 'React.memo demo',
 }
@@ -11,14 +13,19 @@ const NewMessagesCounter = (props: MewMessagesCounterType) => {
     return <div>{props.count}</div>
 }
 const Users = (props: UsersType) => {
+    console.log('USERS')
     return <div>
         {props.users.map((u, i) => <div key={i}>{u}</div>)}
     </div>
 }
 export const Exapmle = () => {
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem']);
+
     return <>
-        <NewMessagesCounter count={10}/>
-        <Users users={['Dimych', 'Valera', 'Artem']}/>
+        <button onClick={() => setCounter(previous => previous + 1)}>+</button>
+        <NewMessagesCounter count={counter}/>
+        <Users users={users}/>
 
     </>
 }
