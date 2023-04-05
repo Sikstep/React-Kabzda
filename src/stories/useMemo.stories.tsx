577676a9999daec5e903477fca450d1bd64f4135
Sikstep <Sikstep1988@gmail.com>
2023-04-05 13:10:10 +0300

    useMemo start

--- src/stories/useMemo.stories.tsx
+++ src/stories/useMemo.stories.tsx
@@ -5,18 +5,29 @@ export default {
 }
 
 export const Example1 = () => {
-    const [a, setA] = useState<number>(0);
-    const [b, setB] = useState<number>(0);
+    const [a, setA] = useState<number>(5);
+    const [b, setB] = useState<number>(5);
 
+    let resultA = 1;
+    let resultB = 1;
+
+    for (let i = 1; i <= a; i++) {
+        resultA *= i;
+    }
+
+    for (let i = 1; i <= b; i++) {
+        resultB *= i;
+    }
+    
     return <>
         <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
         <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
         <hr/>
         <div>
-            Result for a: {a}
+            Result for a: {resultA}
         </div>
         <div>
-            Result for b: {b}
+            Result for b: {resultB}
         </div>
     </>
 
