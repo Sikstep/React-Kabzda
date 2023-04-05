48ae73e525d52135e19938ba9cbf4bd0e467b883
Sikstep <Sikstep1988@gmail.com>
2023-04-05 13:39:06 +0300

    useMemo start defend React.memo by useMemo

--- src/stories/useMemo.stories.tsx
+++ src/stories/useMemo.stories.tsx
@@ -1,29 +1,34 @@
-import React, {useState} from 'react'
+import React, {useMemo, useState} from 'react'
 
 export default {
     title: 'useMemo',
 }
 
-export const Example1 = () => {
+export const DifficultCountingExample = () => {
     const [a, setA] = useState<number>(5);
     const [b, setB] = useState<number>(5);
 
     let resultA = 1;
     let resultB = 1;
 
-    for (let i = 1; i <= a; i++) {
-        let fake = 0;
-        while(fake < 100000000) {
-            fake++;
-            const fakeValue = Math.random();
+    resultA = useMemo(() => {
+        let tempResultA = 1;
+        for (let i = 1; i <= a; i++) {
+            let fake = 0;
+            while (fake < 10000000) {
+                fake++;
+                const fakeValue = Math.random();
+            }
+            tempResultA *= i;
         }
-        resultA *= i;
-    }
+        return tempResultA
+    }, [a])
+
 
     for (let i = 1; i <= b; i++) {
         resultB *= i;
     }
-    
+
     return <>
         <input value={a} onChange={(e) => setA(Number(e.currentTarget.value))}/>
         <input value={b} onChange={(e) => setB(+e.currentTarget.value)}/>
@@ -38,3 +43,27 @@ export const Example1 = () => {
 
 
 }
+
+
+const UsersSecret = (props: {users: string[]}) => {
+    console.log('USERS SECRET')
+    return <div>
+        {props.users.map((u, i) => <div key={i}>{u}</div>)}
+    </div>
+}
+
+const Users = React.memo(UsersSecret)
+export const Exapmle = () => {
+    const [counter, setCounter] = useState(0);
+    const [users, setUsers] = useState(['Dimych', 'Valera', 'Artem', 'Katya']);
+
+
+    return <>
+        <button onClick={() => setCounter(previous => previous + 1)}>+</button>
+        {counter}
+        <Users users={users}/>
+
+    </>
+}
+
+
